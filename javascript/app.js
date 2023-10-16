172acab1e5479126cae345ba6d66e401894a8aca
Tanveer177 <tanveerulhassan2018@gmail.com>
2023-10-16 11:30:21 +0500

    akhuwat lab work

---
+++ javascript/app.js
@@ -0,0 +1,78 @@
+//alert("hello world");
+
+//var a = 5;
+//var b = 4;
+//var c = a+b;
+//alert(c);
+
+//var a = 5;
+//var b = 4;
+//var c = a-b;
+//alert(c);
+
+//var a = 5;
+//var b = 4;
+//var c = a*b;
+//alert(c);
+
+//var a = 5;
+//var b = 4;
+//var c = a/b;
+//alert(c);
+
+///var a = 5;
+//var b = a++;
+//var c = a
+//alert(c);
+
+//var a = 5;
+//var b = ++a;
+//var c = a;
+//alert(c);
+
+//var a = "ali";
+//var b = 45;
+//var c = a;
+//alert(c);
+
+//var a = 5;
+//var b = ali;
+//var c = (a+b) * a;
+//alert(c);
+
+
+
+// var b = 10 % 3;
+// alert(b);
+
+// var message = "Thank for your input";
+// alert(message);
+
+// var a = prompt( "Enter your values ", " your value is:");
+// var b = +a + 10;
+// console.log("the output is: ", " " , b )
+
+
+// var x =" enter your value ";
+// var y = " your values is:"
+// var  a = +prompt("Enter your value ", " your value is: ");
+// var b = a + 10;
+// console.log("the output is", + " " + b )
+
+
+
+// var x =" enter your value ";
+// var y = " your values is:"
+// var  a = +prompt(x,y);
+// var b = a + 10;
+// console.log("the output is", + " " + b )
+
+
+var x =" enter your value ";
+var y = " your values is:"
+var  a = +prompt(x,y);
+var b = a + 10;
+document.write("the output is" + " " + b )    //object model k through 
+
+
+
